c17a671a8d21e4d3bfa917073bf027e2de0ef768
William Lam <williamntlam@gmail.com>
2025-11-05 10:31:07 -0500

    delete: removed github extraction

--- etl_pipeline/extractors/__init__.py
+++ etl_pipeline/extractors/__init__.py
@@ -2,10 +2,8 @@
 
 from extractors.base import BaseExtractor
 from extractors.filesystem_extractor import FilesystemExtractor
-from extractors.github_extractor import GitHubExtractor
 
 __all__ = [
     "BaseExtractor",
     "FilesystemExtractor",
-    "GitHubExtractor",
 ]
