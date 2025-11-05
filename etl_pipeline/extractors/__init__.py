2f892076e85c32cc4031d200b00f37ebc7b77930
William Lam <williamntlam@gmail.com>
2025-11-05 10:29:28 -0500

    delete: removed s3 extraction

--- etl_pipeline/extractors/__init__.py
+++ etl_pipeline/extractors/__init__.py
@@ -3,11 +3,9 @@
 from extractors.base import BaseExtractor
 from extractors.filesystem_extractor import FilesystemExtractor
 from extractors.github_extractor import GitHubExtractor
-from extractors.s3_extractor import S3Extractor
 
 __all__ = [
     "BaseExtractor",
     "FilesystemExtractor",
     "GitHubExtractor",
-    "S3Extractor",
 ]
